9f047225bda2dc61b205a95611b89f3e4722b25d
ardahel <alextibo1@gmail.com>
2025-04-12 23:02:57 -0400

    feat: enable multiplayer sync in main.js

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+// main.js
 import { player, keys, updatePlayer, drawPlayer } from './player.js';
 import { bgImage, platforms, gravity } from './map.js';
 import { setupKeyboard } from './game.js';
@@ -10,26 +11,28 @@ let otherPlayers = {};
 
 function update() {
   updatePlayer(player, gravity, platforms, keys, canvas.height);
+
   socket.emit('move', {
     x: player.x,
-    y: player.y
+    y: player.y,
+    username: getUsername()
   });
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   if (bgImage.complete) {
     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
   }
 
-  // Affiche les autres joueurs
-  Object.values(otherPlayers).forEach(p => {
+  for (const p of Object.values(otherPlayers)) {
     ctx.fillStyle = 'blue';
     ctx.fillRect(p.x, p.y, player.width, player.height);
     ctx.fillStyle = 'black';
     ctx.font = '14px Arial';
     ctx.fillText(p.username, p.x, p.y - 5);
-  });
+  }
 
   drawPlayer(ctx, player, getUsername());
 }
